8f6f659cdb269f547ceed3ac46d940dd3dd97fdf
Nakize <wiktor.slodkowski.2004@gmail.com>
2023-11-25 18:22:58 +0100

    Dodano dodatkową funkcjonalność do /keyboard.. która nie działa

--- Paroxysm/Hooks/Actions/KeyboardAction.cs
+++ Paroxysm/Hooks/Actions/KeyboardAction.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Discord;
 using Discord.WebSocket;
 using Paroxysm.Discord;
@@ -10,56 +9,112 @@ namespace Paroxysm.Hooks.Actions;
 
 public static class KeyboardAction
 {
-    private static readonly string pattern = @"<(?<TextA>(ctrl|alt|shift))\+(?<TextB>[^>]*)>";
+    private static readonly string pattern = @"{([^{}]+)}";
+    private static InputSimulator simulator = new();
 
-    private static List<Tuple<string, string>> FindAndSplit(string input)
+    private static (string modifiedText, List<string> placeholders) GetData(string input)
     {
-        List<Tuple<string, string>> result = new List<Tuple<string, string>>();
+        var placeholders = new List<string>();
 
-        Regex regex = new(pattern);
+        Regex regex = new Regex(pattern);
 
-        MatchCollection matches = regex.Matches(input);
-
-        foreach (Match match in matches)
+        string result = regex.Replace(input, match =>
         {
-            string type = match.Groups["textA"].Value;
-            string keys = match.Groups["textB"].Value;
-            
-            result.Add(new Tuple<string, string>(type, keys));
-        }
+            placeholders.Add(match.Groups[1].Value);
 
-        return result;
-    }
-
-    private static void SimulateKeyCombination(char[] keys, VirtualKeyCode modifierKey)
-    {
-        InputSimulator simulator = new();
-
-        simulator.Keyboard.KeyDown(modifierKey);
-        foreach (var key in keys)
-        {
-            simulator.Keyboard.TextEntry(key);
-        }
+            return "{" + (placeholders.Count - 1) + "}";
+        });
         
-        Thread.Sleep(50);
-
-        simulator.Keyboard.KeyUp(modifierKey);
+         return (result, placeholders);
     }
     
     private static void SimulateKeyPress(string inputText)
     {
-        InputSimulator simulator = new();
-
         foreach (char c in inputText)
         {
             simulator.Keyboard.TextEntry(c);
+        }
+    }
+
+    private static void SimulateSpecialKeyPress(VirtualKeyCode keyCode)
+    {
+        simulator.Keyboard.KeyPress(keyCode);
+    }
+
+    private static VirtualKeyCode GetFunctionKey(string key)
+    {
+        if(Enum.TryParse<VirtualKeyCode>(key, true, out var keyCode))
+        {
+            return keyCode;
+        }
+        else
+        {
+            throw new ArgumentException($@"Unknown Function Key: F{key}");
+        }
+    }
+    
+    private static VirtualKeyCode GetCodeFromString(string key)
+    {
+        switch (key.ToLower())
+        {
+            case ("enter") : return VirtualKeyCode.RETURN;
+            case ("backspace") : return VirtualKeyCode.BACK;
+            case ("tab") : return VirtualKeyCode.TAB;
+            case ("shift") : return VirtualKeyCode.SHIFT;
+            case ("capslock") : return VirtualKeyCode.CAPITAL;
+            case ("ctrl") : return VirtualKeyCode.CONTROL;
+            case ("windows") : return VirtualKeyCode.LWIN;
+            default: throw new ArgumentException($@"Unknown special key: {key.ToLower()}");
+        }
+    }
+    
+    private static void SimulateSequence(IEnumerable<VirtualKeyCode> keyCodes)
+    {
+        foreach (var keyCode in keyCodes)
+        {
+            simulator.Keyboard.KeyDown(keyCode);
+        }
+        
+        Thread.Sleep(50);
+        
+        foreach (var keyCode in keyCodes)
+        {
+            simulator.Keyboard.KeyUp(keyCode);
+        }
+    }
+
+    private static void SimulateSequenceWithSpecial(string text, IEnumerable<string> list)
+    {
+        int index = 0;
+        string[] fragments = text.Split('{', '}');
+
+        foreach (var fragment in fragments)
+        {
+            if (fragment.Length > 0)
+            {
+                if (int.TryParse(fragment, out int sequenceNumber))
+                {
+                    // sequence Number - znak wewnątrz {int}
+                    var data = (list as List<string>)[sequenceNumber];
+                }
+            }
         }
     }
     
     public static Embed Follow(SocketSlashCommand slashCommand)
     {
         string inputText = slashCommand.Data.Options.ElementAt(0).Value as string;
-        SimulateKeyPress(inputText);
+        var result = GetData(inputText);
+
+        if (result.placeholders.Count != 0)
+        {
+            // there are special keys
+            SimulateSequenceWithSpecial(result.modifiedText, result.placeholders);
+        }
+        else
+        {
+            SimulateKeyPress(inputText);
+        }
 
         return DiscordEmbed.CreateWithText(Color.Blue, "Zakończono", $"Wypisano znaki {inputText}",
             Environment.UserName, null);
